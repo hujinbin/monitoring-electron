672557d229ae940dde5e3cb37ee7b84c972f9c83
hujinbin <437044048@qq.com>
2022-08-21 14:00:25 +0800

    feat: 浏览器加载功能

--- preload.js
+++ preload.js
@@ -1,12 +1,19 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+  const input = document.getElementById("input");
+  const ifm = document.getElementById("iframe");
+  input.addEventListener("keydown", function(evt){
+    if(evt.key==="Enter"){    //当按下回车键时
+      updateURL(ifm,input.value)
+      console.log(ifm)
+    }
+  })
 })
+
+function updateURL(ifm, url){
+  if (url.slice(0, 8).toLowerCase() != 'https://' 
+    && url.slice(0, 7).toLowerCase() != 'http://')
+    url = 'https://'+ url;
+  ifm.src = url   //更新ifm的地址
+}
--- renderer.js
+++ renderer.js
@@ -4,3 +4,12 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+function changeFrameHeight(){
+    var ifm= document.getElementById("iframe");
+      ifm.height=document.documentElement.clientHeight-50;
+}
+
+window.addEventListener('resize', function(){
+    changeFrameHeight();
+})
